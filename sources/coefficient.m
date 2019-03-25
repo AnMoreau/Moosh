8f66b69a195250d04c127157ce7e611318646b07
Antoine Moreau <antoine.moreau@uca.fr>
2019-03-25 20:19:09 +0100

    Bug fix.

--- sources/coefficient.m
+++ sources/coefficient.m
@@ -1,4 +1,4 @@
-1%
+%
 % [r,R,t,T]=coefficient(theta,lambda,polarization)
 %
 % This function computes the reflection and transmission coefficients
@@ -46,7 +46,7 @@ if (g>2)
   gamma(2:g-1)=gamma(2:g-1).*(1-2*(imag(gamma(2:g-1))<0));
 end
 % Outgoing wave condition for the last medium
-if (real(Epsilon(Type(g)))<0) && (real(Mu(Type(g)))<0) && (real(sqrt(Epsilon(Type(g))*Mu(Type(g))*k0^2-alpha^2))~=0) 
+if (real(Epsilon(Type(g)))<0) && (real(Mu(Type(g)))<0) && (real(sqrt(Epsilon(Type(g))*Mu(Type(g))*k0^2-alpha^2))~=0)
   gamma(g)=-sqrt(Epsilon(Type(g))*Mu(Type(g))*k0^2-alpha^2);
 else
   gamma(g)=sqrt(Epsilon(Type(g))*Mu(Type(g))*k0^2-alpha^2);
@@ -55,28 +55,28 @@ end
 T{1}=[0,1;1,0];
 %S matrices computation
 for k=1:g-1
-% Layer scattering matrix 
-  t=exp(i*gamma(k)*hauteur(k));    
+% Layer scattering matrix
+  t=exp(i*gamma(k)*hauteur(k));
   T{2*k}=[0,t;t,0];
 % Interface scattering matrix
   b1=gamma(k)/f(Type(k));
   b2=gamma(k+1)/f(Type(k+1));
-  T{2*k+1}=[b1-b2,2*b2;2*b1,b2-b1]/(b1+b2);  
+  T{2*k+1}=[b1-b2,2*b2;2*b1,b2-b1]/(b1+b2);
 end
-% Scattering matrix for the last layer  
-t=exp(i*gamma(g)*hauteur(g));    
+% Scattering matrix for the last layer
+t=exp(i*gamma(g)*hauteur(g));
 T{2*g}=[0,t;t,0];
 % Once the scattering matrixes have been prepared, now let us combine them
-  
-A{1}=T{1};  
+
+A{1}=T{1};
 for j=1:size(T,2)-2
-  A{j+1}= cascade(A{j},T{j+1}); 	
+  A{j+1}= cascade(A{j},T{j+1});
 end
-  
+
 % reflection coefficient of the whole structure
-r=A{size(A,2)}(1,1); 
+r=A{size(A,2)}(1,1);
 % transmission coefficient of the whole structure
-t=A{size(A,2)}(2,1); 
+t=A{size(A,2)}(2,1);
 % Energy reflexion coefficient;
 R=abs(r)^2;
 % Energy transmission coefficient;
